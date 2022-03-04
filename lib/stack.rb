1c88877b067f7a14291c041669297e6e5aa1b53e
Sid Saythongphet <sid.saythongphet@gmail.com>
2022-03-04 13:07:06 -0500

    commit tree 3b7b4e81b9dd001706f984e5458b87964937b054

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,44 @@
-# your code here
+class Stack
+
+    def initialize limit = nil
+        @stack = []
+        @limit = limit
+    end
+
+    def push value
+        if self.full?
+            raise 'Stack Overflow'
+        else
+            @stack.push value
+        end
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        @stack.last
+    end
+
+    def size
+        @stack.length
+    end
+
+    def empty?
+        @stack.empty?
+    end
+
+    def full?
+        @limit == @stack.size
+    end
+
+    def search value
+        @stack.each_with_index do |item, index|
+            if item == value then return size - index - 1
+            end
+        end
+        -1
+    end
+
+end
